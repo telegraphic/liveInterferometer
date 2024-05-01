464f889ca111b96dfd2ab376053be9009ea4fa9f
Danny Price <danny.price@skao.int>
2024-05-01 12:39:33 +0800

    Tidy-up and docstrings

--- interferometer_sim.py
+++ interferometer_sim.py
@@ -1,21 +1,167 @@
+"""
+# Interferometer simulator, using webcam
+
+Based on 'liveinterferometer' by Jack Hickish, Griffin Foster, and Charles Copley
+https://github.com/jack-h/liveInterferometer
+
+"""
 import cv2
 import numpy as np
 from dash import Dash, dcc, html, Input, Output
 import dash_bootstrap_components as dbc
 import plotly.express as px
 
+
+##############
+## OpenCV camera + imaging methods
+##############
+
 def test_camera(cam: cv2.VideoCapture):
+    """ Tests the camera is working by capturing an image
+
+    Args:
+        cam (cv2.VideoCapture): Camera device to test
+    """
     rv, img = cam.read()
     if img is None or rv is False:
         raise RuntimeError("Camera not working")
 
-def load_target_image(filepath: str):
-    target_image = cv2.imread(filepath)
-    target_img_grey = cv2.cvtColor(target_image, cv2.COLOR_BGR2GRAY)
-    target_img_grey[target_img_grey < 100] = 0
-    return target_img_grey
+def get_aspect_ratio(cam: cv2.VideoCapture) -> float:
+    """ Returns the aspect ratio for the camera
 
-def gauss_grid(xsize, ysize):
+     Args:
+        cam (cv2.VideoCapture): Camera device to query
+
+    Returns:
+         aspect_ratio (float): Aspect ratio of camera frames
+    """
+    rv, img = cam.read()
+    return img.shape[1] / img.shape[0]
+
+def read_from_camera(cam: cv2.VideoCapture, xsize: int, ysize: int, greyscale: bool=False) -> np.ndarray:
+    """ Read a frame from the camera
+
+     Args:
+        cam (cv2.VideoCapture): Camera device
+        xsize (int): Image output width, in pixels
+        ysize (int): Image output height, in pixels
+        greyscale (bool): Converts image to greyscale if set to True.
+
+    Returns:
+         img (np.ndarray): Image as a numpy array.
+                           If grayscale, image has shape (ysize, xsize)
+                           If color (RGB), image has shape (ysize, xsize, 3)
+    """
+    # To avoid buffering issues, we read a few frames at once
+    for i in range(4):
+        rv, img = cam.read()
+    img = cv2.resize(img, (xsize, ysize), interpolation=cv2.INTER_CUBIC)
+    if greyscale:
+        img = to_greyscale(img)
+    else:
+        img = to_rgb(img)
+    img = np.fliplr(img)
+    return img
+
+def to_greyscale(img: np.ndarray) -> np.ndarray:
+    """ Converts an image to greyscale, using CV2"""
+    return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+def to_rgb(img: np.ndarray) -> np.ndarray:
+    """ Converts a BGR (webcam default) image to RGB (computer default) """
+    return cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+def load_image(filepath: str, xsize: int=None, ysize: int=None, greyscale: bool=False) -> np.ndarray:
+    """ Loads target image using CV2
+
+    Args:
+        filepath (str): Path to file to load
+        xsize (int): If set, will resize image to be xsize pixel wide.
+                     Note: ysize must be set too.
+        ysize (int): If set, will resize image to be ysize pixels high.
+                     Note: xsize must be set too.
+        greyscale (int): Convert to greyscale if True (default False)
+
+    Returns:
+        img (np.ndarray): Image as a numpy array
+    """
+    try:
+        img = cv2.imread(filepath)
+        if xsize is not None:
+            img = cv2.resize(img, (xsize, ysize), interpolation=cv2.INTER_CUBIC)
+        if greyscale:
+            img = to_greyscale(img)
+            #img[img < 100] = 0
+        else:
+            img = to_rgb(img)
+    except cv2.error:
+        raise ValueError(f"Could not load {filepath}")
+    return img
+
+def detect_stations(img: np.ndarray, xsize: int, ysize: int, rescale_factor: int=4) -> tuple:
+        """ Detect station locations in an image using a Hough Circle detection algorithm
+
+        Station locations are represented in the image by circles (intended for coins)
+        Note that the circle detection is a bit flaky, and may need some hand tuning.
+
+        Args:
+            img (np.ndarray): Input image with circles corresponding to station locations
+            xsize (int): Image width, in pixels
+            ysize (int): Image height, in pixels
+            rescale_factor (int): Rescale down by this factor (useful for PSF compute speed)
+
+        Returns:
+            station_locs (np.ndarray): Station locations array,
+                                       Array shape is (ysize // rescale_factor, xsize // rescale_factor)
+            station_overlay (np.ndarray): Basic image showing detected station locations
+                                       Array shape is (ysize, xsize)
+        """
+
+        img = 255 - img
+        station_overlay = np.zeros_like(img)
+        station_locs = np.zeros([ysize // rescale_factor + 1, xsize // rescale_factor + 1])
+
+        min_rad = int(xsize // 40 / 2)
+        max_rad = int(xsize // 10 / 2)
+        min_dist = 2 * min_rad
+
+        #cv2.HoughCircles(image, method, dp, minDist, circles, param1, param2, minRadius, maxRadius)
+        #   image: input webcam image size
+        #   method: must be cv2.CV_HOUGH_GRADIENT exists
+        #   dp: Inverse ratio of the accumulator resolution to the image resolution. this basically affects the min/max radius
+        #   minDist: Minimum distance between the centers of the detected circles. If the parameter is too small, multiple neighbor circles may be falsely detected in addition to a true one. If it is too large, some circles may be missed.
+        #   circles: set to None
+        #   param1: threshold parameter
+        #   param2: The smaller it is, the more false circles may be detected.
+        #   minRadius: Minimum circle radius
+        #   maxRadius: Maximum circle radius
+        circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, dp=1.1, minDist=min_dist,
+                                   param1=50, param2=30, minRadius=min_rad, maxRadius=max_rad)
+
+        if circles is not None:
+            for cn,circle in enumerate(circles[0]):
+                y,x = circle[1],circle[0]
+                try:
+                    station_overlay[int(y-5):int(y+5), int(x-5):int(x+5)] = 1
+                except:
+                    pass
+                station_locs[int(y // rescale_factor), int(x // rescale_factor)] = 1
+        else:
+                station_overlay[int(ysize/2-5):int(ysize/2+5), int(xsize/2-5):int(xsize/2+5)] = 1
+                station_locs[int(ysize/2 // rescale_factor), int(xsize/2 // rescale_factor)] = 1
+        return station_locs, station_overlay
+
+##############
+## Interferometer maths
+##############
+
+def gauss_grid(xsize: int, ysize: int) -> np.ndarray:
+    """ Create a Gaussian grid to modulate the PSF with
+
+    Args:
+        xsize (int): Grid width in pixels
+        ysize (int): Grid height in pixels
+    """
     # make a 2D Gaussian to modulate the PSF with
     def gauss2d(x0, y0, amp, stdx, stdy):
         return lambda x, y: amp * np.exp(-1. * ((((x - x0)**2.) / (2 * stdx**2.)) +
@@ -28,6 +174,23 @@ def gauss_grid(xsize, ysize):
     return gaussGrid
 
 def fast_conv(image, psf):
+    """ Convolve an image with a PSF (point spread function)
+
+    By the convolution theorem, using '*' to denote convolution operation,
+    and '.' to denote point-wise multiplication, we have
+
+        dirty_image = clean_image * psf = InverseFourierTransform ( CLEAN_IMAGE . PSF )
+
+    where CLEAN_IMAGE and PSF are the fourier transforms of clean_image and psf.
+
+    Args:
+        image (np.ndarray): Input image.
+        psf (np.ndarray): Point spread function
+
+    Returns:
+        dirty_image (np.ndarray): 'Dirty' input image after convolution with PSF
+
+    """
     max_size = np.array([np.max([image.shape[0], psf.shape[0]]),
                          np.max([image.shape[1], psf.shape[1]])])
     n = int(2**np.ceil(np.log2(max_size[0])))
@@ -36,111 +199,108 @@ def fast_conv(image, psf):
     return imageDirty[psf.shape[0] // 2:image.shape[0] + psf.shape[0] // 2,
                       psf.shape[1] // 2:image.shape[1] + psf.shape[1] // 2]
 
-def read_from_camera(cam, xsize, ysize):
-    for i in range(4):
-        rv, layout_img = cam.read()
+def compute_psf(station_locs: np.ndarray, xsize: int, ysize: int, gauss_grid: np.ndarray) -> np.ndarray:
+    """ Compute the point spread function (PSF) for a given station layout
 
-    layout_img = cv2.resize(layout_img, (ysize, xsize), interpolation=cv2.INTER_CUBIC)
-    layout_img_grey = cv2.cvtColor(layout_img, cv2.COLOR_BGR2GRAY)
-    layout_img_grey = np.fliplr(layout_img_grey)
-    return layout_img_grey
+    The PSF tells us how the telescope would respond to a point source of radiation:
+    power from the point will leak, or 'spread' into other pixels.
 
-def detect_stations(img, ysize, xsize, rescale_factor=4):
-        #cv2.HoughCircles(image, method, dp, minDist, circles, param1, param2, minRadius, maxRadius)
-        #   image: input webcam image size
-        #   method: only cv.CV_HOUGH_GRADIENT exists
-        #   dp: Inverse ratio of the accumulator resolution to the image resolution. this basically affects the min/max radius
-        #   minDist: Minimum distance between the centers of the detected circles. If the parameter is too small, multiple neighbor circles may be falsely detected in addition to a true one. If it is too large, some circles may be missed.
-        #   circles: set to None
-        #   param1: threshold parameter
-        #   param2: The smaller it is, the more false circles may be detected.
-        #   minRadius: Minimum circle radius
-        #   maxRadius: Maximum circle radius
-        img = 255 - img
-        station_overlay = np.zeros_like(img)
-        station_locs = np.zeros([ysize // rescale_factor, xsize // rescale_factor])
+    The PSF is computed by taking the Fourier transform of the station locations.
+    For an interferometer, adding more stations will improve the PSF, making images cleaner.
 
-        circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, 2, 20, param1=50, param2=30, minRadius=50, maxRadius=128)
+    Args:
+        station_locs (np.ndarray): Station locations, as a 2D numpy array.
+        xsize (int): Station image width, in pixels
+        ysize (int):  Station image height, in pixels
+        gauss_grid (np.ndarray): A Gaussian grid that is modulated with the PSF
 
-        if circles is not None:
-            print(len(circles[0]))
-            for cn,circle in enumerate(circles[0]):
-                x,y = circle[1],circle[0]
-
-                try:
-                    station_overlay[int(x-5):int(x+5),int(y-5):int(y+5)] = 1
-                except:
-                    pass
-                station_locs[int(x // rescale_factor), int(y // rescale_factor)]=1
-
-        return station_locs, station_overlay
-
-def compute_psf(station_locs, xsize, ysize, gauss_grid):
-        psf = np.fft.fftshift(np.abs(np.fft.fft2(station_locs, s=[ysize, xsize]))**2)
-        psf_norm = (psf * gauss_grid / psf.max())
-        return psf_norm
+    Returns:
+        psf_norm (np.ndarray): Point spread function multiplied by Gaussian.
+                               Normalized so maximum value is 1.
+    """
+    psf = np.fft.fftshift(np.abs(np.fft.fft2(station_locs, s=[ysize, xsize]))**2)
+    psf_norm = (psf * gauss_grid / psf.max())
+    return psf_norm
 
 
+if __name__ == "__main__":
 
-CAMERA_DEVICE_INDEX = 0
-XSIZE, YSIZE = (480, 640)
+    CAMERA_DEVICE_INDEX = 0          # For my mac, the webcam is showing up as id=0
+    MAXSIZE = 640                    # Maximum image size. If set too high, things get slow.
 
-cam0 = cv2.VideoCapture(CAMERA_DEVICE_INDEX)
+    # Create a camera object
+    cam0 = cv2.VideoCapture(CAMERA_DEVICE_INDEX)
+    test_camera(cam0)
 
-test_camera(cam0)
-gauss_grid = gauss_grid(XSIZE, YSIZE)
+    # Set the aspect ratio and compute image shape
+    aspect_ratio = get_aspect_ratio(cam0)
+    XSIZE = MAXSIZE
+    YSIZE = int(MAXSIZE / aspect_ratio)
 
-target_img = load_target_image('astro_test_image.jpg')
-layout_img = read_from_camera(cam0, XSIZE, YSIZE)
-station_locs, station_overlay = detect_stations(layout_img, XSIZE, YSIZE)
-psf = compute_psf(station_locs, XSIZE, YSIZE, gauss_grid)
-dirty_img = fast_conv(target_img, psf)
+    # Precompute a Gaussian grid for PSF
+    gauss_grid = gauss_grid(XSIZE, YSIZE)
 
-fig_layout = px.imshow(layout_img, binary_string=True)
-fig_target = px.imshow(target_img, binary_string=True)
-fig_dirty  = px.imshow(dirty_img, binary_string=True)
+    # Now, start the web app
+    app = Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
 
-app = Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
+    # This callback updates the images on a timed interval
+    @app.callback(
+        Output("plots", "children"),
+        Input('timer', 'n_intervals'))
+    def update_webcam(t_counter):
 
-@app.callback(
-    Output("plots", "children"),
-    Input('timer', 'n_intervals'))
-def update_webcam(t_counter):
-    layout_img = read_from_camera(cam0, XSIZE, YSIZE)
-    target_img = load_target_image('astro_test_image.jpg')
-    layout_img = read_from_camera(cam0, XSIZE, YSIZE)
-    station_locs, station_overlay = detect_stations(layout_img, XSIZE, YSIZE)
-    psf = compute_psf(station_locs, XSIZE, YSIZE, gauss_grid)
-    dirty_img = fast_conv(target_img, psf)
+        # Capture layout image from camera, and load target sky image
+        layout_img = read_from_camera(cam0, XSIZE, YSIZE)
+        target_img = load_image('images/gleam.jpg', XSIZE, YSIZE)
 
-    fig_layout = px.imshow(layout_img, color_continuous_scale='gray')
-    fig_target = px.imshow(target_img, color_continuous_scale='gray')
-    fig_dirty  = px.imshow(dirty_img, color_continuous_scale='gray')
-    fig_station_locs = px.imshow(station_locs, color_continuous_scale='gray')
+        # Detect stations, compute the PSF for the station, and create a dirty (convolved) image
+        station_locs, station_overlay = detect_stations(to_greyscale(layout_img), XSIZE, YSIZE)
+        psf = compute_psf(station_locs, XSIZE, YSIZE, gauss_grid)
+        dirty_img = fast_conv(to_greyscale(target_img), psf)
 
-    for fig in (fig_layout, fig_target, fig_dirty, fig_station_locs):
-        fig.update_layout(coloraxis_showscale=False)
-        fig.update_xaxes(showticklabels=False)
-        fig.update_yaxes(showticklabels=False)
+        # Convert images into Plotly imshows for display in the app
+        fig_layout = px.imshow(layout_img, color_continuous_scale='gray', title='Antenna Layout (webcam)')
+        fig_target = px.imshow(target_img, color_continuous_scale='gray', title='Actual sky')
+        fig_dirty  = px.imshow(dirty_img, color_continuous_scale='gray', title='Observed sky')
+        fig_station_locs = px.imshow(station_locs, color_continuous_scale='gray', title='Detected Antennas')
 
-    row1 = html.Tr([html.Td(dcc.Graph(id="antenna_layout", figure=fig_layout)),
-                    html.Td(dcc.Graph(id="station_locs", figure=fig_station_locs))])
-    row2 = html.Tr([html.Td(dcc.Graph(id="target", figure=fig_target)),
-                    html.Td(dcc.Graph(id="dirty", figure=fig_dirty))])
+        # Set some plot layout values so it looks nicer
+        for fig in (fig_layout, fig_target, fig_dirty, fig_station_locs):
+            pdict = {
+                'plot_bgcolor': 'rgba(0, 0, 0, 0)',
+                'paper_bgcolor': 'rgba(0, 0, 0, 0)',
+                'coloraxis_showscale': False,
+                'font': {'color': '#ffffff'},
+                'margin_l': 10,
+                'margin_r': 10,
+                'margin_t': 50,
+                'margin_b': 10
+            }
 
-    children = [html.Tbody([row1, row2])]
+            fig.update_layout(pdict)
+            fig.update_xaxes(showticklabels=False)
+            fig.update_yaxes(showticklabels=False)
 
-    return children
+        # Generate the HTML Table for a 2x2 grid of images
+        row1 = html.Tr([html.Td(dcc.Graph(id="antenna_layout", figure=fig_layout)),
+                        html.Td(dcc.Graph(id="station_locs", figure=fig_station_locs))])
+        row2 = html.Tr([html.Td(dcc.Graph(id="target", figure=fig_target)),
+                        html.Td(dcc.Graph(id="dirty", figure=fig_dirty))])
+        children = [html.Tbody([row1, row2])]
 
-app.layout = html.Div([
-    html.H4('Interferometer simulator'),
-    html.Div(children=update_webcam(0), id='plots'),
-    dcc.Interval(
-        id='timer',
-        interval=1000, # in milliseconds
-        n_intervals=0
-    )
-])
+        return children
 
+    # Set app layout
+    # This is the main Dash layout
+    app.layout = html.Div([
+        html.H4('Interferometer simulator'),
+        html.Div(children=update_webcam(0), id='plots'),
+        dcc.Interval(
+            id='timer',
+            interval=1000, # in milliseconds
+            n_intervals=0
+        )
+    ])
 
-app.run_server(debug=True)
+    # Finally, start the web app running
+    app.run_server(debug=True)
